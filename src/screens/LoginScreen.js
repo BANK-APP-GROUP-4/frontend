826d928d9b35b1ee8118973fc77c7c8cabbd728a
Ritesh Rai <riteshrai997@gmail.com>
2023-09-11 13:21:30 +0530

    Initial Login Feature Added

---
+++ src/screens/LoginScreen.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import AuthService from '../actions/AuthService';
+
+const LoginForm = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await AuthService.login(username, password);
+      
+    } catch (error) {
+      
+    }
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={handleLogin}>
+        <input
+          type="text"
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default LoginForm;
