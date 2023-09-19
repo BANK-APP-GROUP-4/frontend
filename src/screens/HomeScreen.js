ef7cc7d1824323b5911b4f85c3516072589abe9f
preksha492 <65268607+preksha492@users.noreply.github.com>
2023-09-19 22:29:50 +0530

    Update HomeScreen.js

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,24 +1,46 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Container, Typography, Button, Grid } from "@mui/material";
+import { LockOpen } from "@mui/icons-material";
 
 const HomeScreen = () => {
-    const isLoggedIn = localStorage.getItem('customer_token') ? true : false;
+    const isLoggedIn = localStorage.getItem("customer_token") ? true : false;
+
     return (
-        <div style={{textAlign:"center", border:"1px solid black", padding:"10px", margin: "5rem"}}>
-            
-        <h2>Welcome to the NeoFinTech Bank</h2>
-        <div>
-            {isLoggedIn ? <Link to="/customer/dashboard">Dashboard</Link> : 
-            <div>
-                <div style={{margin: "2rem"}}>
-                <Link to="/customer/login">Login</Link>
-                </div>
-                <Link to="/customer/register">Register</Link>
-            </div>
-            }
-        </div>
-        </div>
+        <Container maxWidth="md" style={{ textAlign: "center", border: "1px solid black", padding: "20px", marginTop: "5rem" }}>
+            <Typography variant="h2">Welcome to the NeoFinTech Bank</Typography>
+            <Grid container spacing={2} justifyContent="center">
+                {isLoggedIn ? (
+                    <Grid item>
+                        <Button component={Link} to="/customer/dashboard" variant="contained" color="primary">
+                            Dashboard
+                        </Button>
+                    </Grid>
+                ) : (
+                    <Grid item>
+                        <Button component={Link} to="/customer/login" variant="contained" color="primary">
+                            Login
+                        </Button>
+                    </Grid>
+                )}
+                <Grid item>
+                    <Button component={Link} to="/customer/register" variant="outlined" color="primary">
+                        Register
+                    </Button>
+                </Grid>
+            </Grid>
+            <Button
+                component={Link}
+                to="/admin/login"
+                variant="contained"
+                color="secondary"
+                style={{ position: "absolute", top: "20px", right: "20px" }}
+                startIcon={<LockOpen />}
+            >
+                Admin Login
+            </Button>
+        </Container>
     );
-}
+};
 
 export default HomeScreen;
