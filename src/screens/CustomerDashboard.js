590cf15b846aed8ac94eda9875a5390344d2b72e
Ritesh Rai <riteshrai997@gmail.com>
2023-09-21 03:42:19 +0530

    Updated customer dashboard

--- src/screens/CustomerDashboard.js
+++ src/screens/CustomerDashboard.js
@@ -1,44 +1,56 @@
 import React, { useState } from 'react';
-import { LogoutService } from '../actions/CustomerActions';
 import { Link } from 'react-router-dom';
 import CustomerNavbar from '../components/CustomerNavbar';
 import {CustomerDetailsService} from '../actions/CustomerActions';
-import Logout from '../components/Logout';
+import { AccountDetailsService } from '../actions/AccountsActions';
 import SummaryPNG from "../assets/summary.png";
+import AccountStatementPNG from "../assets/acc-statement.png";
 import TransferPNG from "../assets/transfer.png";
 import Footer from '../components/Footer';
 
 const CustomerDashboard = () => {
-    // const auth_token = localStorage.getItem('customer_token');
-    // if(!auth_token){
-    //     window.location.href = '/customer/login';
-    // }
-    // else {
-    //     const customer_details = CustomerDetailsService.getCustomerDetails(auth_token);
-    //     localStorage.setItem('customer_details', JSON.stringify(customer_details.customer_details));
-    // }
-    const handleLogout = () => {
-        LogoutService.logout();
+    const auth_token = localStorage.getItem('auth_token');
+    if(!auth_token){
+        window.location.href = '/customer/login';
     }
-    // const customer_details = JSON.parse(localStorage.getItem('customer_details'));
+    else {
+        async function getCustomerDetails() {
+        const customer_details = await CustomerDetailsService.getCustomerDetails(auth_token);
+        if(customer_details.status === "success"){
+            localStorage.setItem('customer_details', JSON.stringify(customer_details.customer_details));
+        }
+    }
+    async function getAccountDetails() {
+        const customer_id = JSON.parse(localStorage.getItem('customer_details')).id;
+        const account_details = await AccountDetailsService.getAccountDetails(auth_token, customer_id);
+        if(account_details.status === "success"){
+            localStorage.setItem('savings_account_details', JSON.stringify(account_details.savingsAccountList));
+            localStorage.setItem('fd_account_details', JSON.stringify(account_details.fdAccountList));
+        }            
+    }
+    getAccountDetails();
+        
+        getCustomerDetails();
+    }
+    const customer_details = JSON.parse(localStorage.getItem('customer_details'));
     
     return (
         <div className="customer-dashboard-container">
             <CustomerNavbar />
             <div className='customer-greeting'>
-                <h2>Welcome,</h2>
-                <h4>Your Account Status is: </h4>
+                <h2>Welcome, {customer_details.firstName} </h2>
             </div>
             <div className='dashboard-card-container'>
-                {/* Available Balance, three cards summary, Transfer Money */}
-                <div className='dashboard-card dashboard-card-bal'>
-                    <h2>Available Balance</h2>
-                    <h3>Rs. 10000</h3>
-                </div>
                 <Link className='dashboard-card' to='/customer/account-summary'>
                         <div className='dashboard-card-img'>
                         <img className='dash-link-png' src={SummaryPNG} alt="Summary" />
                         <h2>Account Summary</h2>
+                        </div>
+                </Link>
+                <Link className='dashboard-card' to='/customer/account-statement'>
+                        <div className='dashboard-card-img'>
+                        <img className='dash-link-png' src={AccountStatementPNG} alt="Summary" />
+                        <h2>Account Statement</h2>
                         </div>
                 </Link>
                 <Link className='dashboard-card' to='/customer/transfer-funds'>
